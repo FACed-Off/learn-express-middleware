502e46346399352e45442be722ee9b32072abab4
Shivani Shah <shivanishah96@hotmail.co.uk>
2020-09-14 13:32:46 +0100

    complete express middleware tutorial

--- workshop/server.js
+++ workshop/server.js
@@ -10,6 +10,11 @@ const server = express();
 server.use(cookieParser());
 server.use(express.urlencoded());
 
+server.use((req, res, next) => {
+  console.log(`${req.method} ${req.url}`);
+  next();
+});
+
 server.get("/", (req, res) => {
   const token = req.cookies.user;
   if (token) {
@@ -42,13 +47,28 @@ server.get("/log-out", (req, res) => {
   res.redirect("/");
 });
 
-server.get("/profile", (req, res) => {
+server.use((req, res, next) => {
   const token = req.cookies.user;
-  const user = jwt.verify(token, SECRET);
-  res.send(`<h1>Hello ${user.email}</h1>`);
+  if (token) {
+    const user = jwt.verify(token, SECRET);
+    req.user = user;
+  }
+  next();
 });
 
-server.get("/profile/settings", (req, res) => {
+function checkAuth(req, res, next) {
+  if (!req.user) {
+    res.status(401).send(`<h1>Please <a href="/log-in">log in</a></h1>`);
+  } else {
+    next();
+  }
+}
+
+server.get("/profile", checkAuth, (req, res) => {
+  res.send(`<h1>Hello ${req.user.email}</h1>`);
+});
+
+server.get("/profile/settings", checkAuth, (req, res) => {
   const token = req.cookies.user;
   const user = jwt.verify(token, SECRET);
   res.send(`<h1>Settings for ${user.email}</h1>`);
@@ -56,7 +76,19 @@ server.get("/profile/settings", (req, res) => {
 
 server.get("/error", (req, res, next) => {
   const fakeError = new Error("uh oh");
+  fakeError.status = 403;
   next(fakeError);
 });
+
+function handleErrors(error, req, res, next) {
+  console.error(error);
+  if (!error.status) {
+    res.status(500).send("<h1>OOPS!<h1>");
+  } else {
+    res.status(error.status).send(`<h1>Something went wrong</h1>`);
+  }
+  //console.log(error.status);
+}
+server.use(handleErrors);
 
 server.listen(PORT, () => console.log(`Listening on http://localhost:${PORT}`));
